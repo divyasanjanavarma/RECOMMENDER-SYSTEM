0d6490381dc1513478a708ec8c6bd90a44f5a4d9
divyasanjanavarma <158834784+divyasanjanavarma@users.noreply.github.com>
2024-06-11 18:11:13 +0530

    Create recommendersystem.java

---
+++ oop pbl/recommendersystem.java
@@ -0,0 +1,25 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reccommendersystem;
+
+/**
+ *
+ * @author chandrika
+ */
+public class ReccommenderSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        mainpage obj=new mainpage();    
+        obj.setLocationRelativeTo(null);                // Set the location of the new frame
+        obj.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
